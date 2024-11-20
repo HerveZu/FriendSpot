e0e8c14c629bfefb8b774c5a98edc568672f79f6
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2024-11-20 23:57:37 +0100

    Add search when querying available parking

--- backend/Api/MySpot/GetAvailableParking.cs
+++ backend/Api/MySpot/GetAvailableParking.cs
@@ -2,11 +2,20 @@ using Api.Common.Infrastructure;
 using Api.MySpot.Contracts;
 using Domain;
 using FastEndpoints;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.MySpot;
 
-internal sealed class GetAvailableParking(AppDbContext dbContext) : EndpointWithoutRequest
+[PublicAPI]
+public sealed record GetAvailableParkingRequest
+{
+    [FromQuery]
+    public string? Search { get; init; }
+}
+
+internal sealed class GetAvailableParking(AppDbContext dbContext) : Endpoint<GetAvailableParkingRequest>
 {
     public override void Configure()
     {
@@ -14,10 +23,20 @@ internal sealed class GetAvailableParking(AppDbContext dbContext) : EndpointWith
         AllowAnonymous();
     }
 
-    public override async Task HandleAsync(CancellationToken ct)
+    public override async Task HandleAsync(GetAvailableParkingRequest req, CancellationToken ct)
     {
-        var availableParking = await dbContext
-            .Set<Parking>()
+        var matchingParking = dbContext.Set<Parking>().AsQueryable();
+        var search = req.Search?.ToLowerInvariant();
+
+        if (!string.IsNullOrWhiteSpace(search))
+        {
+            matchingParking = matchingParking.Where(
+                parking =>
+                    EF.Functions.ILike(parking.Name, $"%{search}%")
+                    || EF.Functions.ILike(parking.Address, $"%{search}%"));
+        }
+
+        var availableParking = await matchingParking
             .OrderBy(parking => parking.Name)
             .Select(parking => parking.ToDto())
             .ToArrayAsync(ct);
