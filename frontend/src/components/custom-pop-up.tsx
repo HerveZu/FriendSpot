086d38a07bb454ddfd4cbfed8964e8032577dbcd
Jimmy Catalano <jimmy.catalano@aura-aero.com>
2024-11-20 16:25:20 +0100

    Adding a custom component for buttons

--- frontend/src/components/custom-pop-up.tsx
+++ frontend/src/components/custom-pop-up.tsx
@@ -9,17 +9,22 @@ import {
   } from "@/components/ui/dialog"
 import { Input } from "./ui/input";
 
-export function BookPlaceBtn() {
+interface ActionBouton {
+    
+    buttonTitle: string, 
+    dialogTitle: string,
+    details: string
+    actionButton: string
+}
 
+export function CustomPopUp({buttonTitle, dialogTitle, details, actionButton}: ActionBouton) {
     return(
-
         <Dialog>
-        <DialogTrigger asChild><Button variant={"outline"} className="w-full text-blue-400 h-14">Réserver une place</Button></DialogTrigger>
-        
+        <DialogTrigger asChild><Button variant={"outline"} className="w-full text-blue-400 h-14">{buttonTitle}</Button></DialogTrigger>
         <DialogContent className="flex flex-col items-center w-10/12 rounded-md">
             <DialogHeader>
-            <DialogTitle>Réserver une place</DialogTitle>
-            <div className="text-sm">3 places disponibles</div>
+            <DialogTitle>{dialogTitle}</DialogTitle>
+                <p className="text-sm">{details}</p>
             <DialogDescription className="flex flex-col gap-4 pt-5 pb-3">
                 <div className="flex items-center gap-2">
                     <p>Du</p>
@@ -28,13 +33,11 @@ export function BookPlaceBtn() {
                 <div className="flex items-center gap-2">
                     <p>Au</p>
                     <Input type="date"></Input>
-                </div>
-                
+                </div>       
             </DialogDescription>
             </DialogHeader>
-            <Button>Je réserve pour 4 crédits</Button>
+            <Button>{actionButton}</Button>
         </DialogContent>
         </Dialog>
-    
     )
 }
--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -1,18 +1,27 @@
-import { BookPlaceBtn } from "@/components/book-place-btn";
-import { Button } from "@/components/ui/button";
+import { CustomPopUp } from "@/components/custom-pop-up";
 
 export function LandingPage() {
     return(
-        <div className="flex flex-col mt-14 gap-5 text-lg px-8 h-[calc(100vh-56px)]">
-            <p>12 places sont actuellement libres
-            dans votre parking</p>
+        <div className="flex flex-col mt-14 gap-5 text-lg px-8">
+            <h1 className="text-xl">Bienvenue sur <span className="text-blue-400">FriendSpot</span> (bêta)</h1>
+            <p className="text-base">12 places sont actuellement libres <br/>  dans votre parking</p>
             <div className="flex flex-col mt-5 gap-5">
-                <BookPlaceBtn/>
-                <Button variant={"outline"} className="text-blue-400 h-14">Prêter ma place</Button>
+                <CustomPopUp
+                  buttonTitle="Réserver une place"
+                  dialogTitle="Réserver une place"
+                  details="3 places disponibles"
+                  actionButton="Je réserve pour 4 crédits"
+                />
+                <CustomPopUp
+                  buttonTitle="Je prête ma place"
+                  dialogTitle="Je prête ma place"
+                  details=""
+                  actionButton="Prêter ma place et gagner 2 crédits"
+                />
             </div>
-            <div className="flex flex-col gap-3 mt-5">
-                <h2>Mes Réservations</h2>
-                <div className="flex flex-col gap-1 border min-h-[50px] rounded-lg px-2.5 py-2">
+            <div className="flex flex-col gap-5 mt-3">
+                <h2>Mes <span className="text-[#60A5FA]">Réservations</span></h2>
+                <div className="flex flex-col gap-1 border min-h-[50px] rounded-lg px-3 py-3">
                     <p className="text-base">Place no 123A</p>
                     <p className="text-xs">Jusqu'au 16 novembre à 20h</p>
                 </div>
