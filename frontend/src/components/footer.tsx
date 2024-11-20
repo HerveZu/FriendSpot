cc8d278764855dfdd82ca74d140df7a2cfa0d96d
Jimmy Catalano <jimmy.catalano@aura-aero.com>
2024-11-20 16:57:52 +0100

    Adding margin bottom under footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return(
-        <div className="flex flex-col gap-4 items-center justify-center text-xs px-10 mt-14">
+        <div className="flex flex-col gap-4 items-center justify-center text-xs px-10 mt-14 mb-10">
             <p className="text-md">Trouvez une place de parking entre voisins.<br/></p>
             <p>&copy; 2024 <span className="text-blue-400">FriendSpot</span> (version de test)</p>
             <p>Réalisé par Hervé & Jimmy</p>
