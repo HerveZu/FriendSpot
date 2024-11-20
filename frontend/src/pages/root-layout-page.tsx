7fe3660566b0fcee1f90b8304048000ae7e4df96
Jimmy Catalano <jimmy.catalano@aura-aero.com>
2024-11-20 16:25:34 +0100

    Adding footer

--- frontend/src/pages/root-layout-page.tsx
+++ frontend/src/pages/root-layout-page.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/header';
+import { Footer } from '@/components/footer';
+import { Separator } from '@/components/ui/separator';
 
 export function RootLayoutPage() {
 	return (
 		<>
 			<Header />
 			<Outlet />
+			<Separator orientation="horizontal" className='mt-14'/>
+			<Footer/>
 		</>
 	);
 }
