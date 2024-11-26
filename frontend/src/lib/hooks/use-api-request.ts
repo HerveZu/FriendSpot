b535d275fdfd5b5de223f58394812448a98c157f
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-11-27 00:15:50 +0100

    Add a new hook for api requests

---
+++ frontend/src/lib/hooks/use-api-request.ts
@@ -0,0 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { useCallback } from 'react';
+
+type httpMethod = 'GET' | 'POST' | 'PUT';
+
+export function useApiRequest() {
+	const { getAccessTokenSilently } = useAuth0();
+
+	const apiRequest = useCallback(async (url: string, method: httpMethod) => {
+		await fetch(url, {
+			method: method,
+			headers: {
+				Authorization: `Bearer ${await getAccessTokenSilently()}`
+			}
+		});
+	}, []);
+	return { apiRequest };
+}
