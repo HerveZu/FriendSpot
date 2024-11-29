4a9d4751685f3e830004427d1059bedd706cb73d
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-11-29 01:59:09 +0100

    Add a fetch to retrieve a user's credits

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -9,44 +9,67 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Separator } from '@/components/ui/separator';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
+import { useApiRequest } from '@/lib/hooks/use-api-request';
 
 export function Header() {
-	const { loginWithRedirect } = useAuth0();
+	const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+	const { apiRequest } = useApiRequest();
 
-	async function signup() {
-		const loginResponse = await loginWithRedirect();
-		console.log(loginResponse);
-	}
+	const baseUrl = import.meta.env.VITE__AUTH0__BASE__URL;
+
+	const [userCredit, setUserCredit] = useState({});
+	const currentCredit = userCredit?.wallet?.credits;
+	const pendingCredit = userCredit?.wallet?.pendingCredits;
+
+	useEffect(() => {
+		async function fetchUserCredit() {
+			const response = await apiRequest(`${baseUrl}/@me/status`, 'GET');
+			const data = await response.json();
+			setUserCredit(data);
+		}
+		fetchUserCredit();
+	}, []);
 
 	return (
-		<div className="flex justify-between items-center px-4 py-2">
+		<div className="flex justify-between items-center px-4 py-2 min-h-[80px] z-10 ">
 			<Link to={'/'}>
 				<Car width={28} height={28} color="#60A5FA" />
 			</Link>
 			<div className="flex items-center gap-3">
-				<p>32 crédits</p>
+				{currentCredit && (
+					<div>
+						<p className="text-">{`${currentCredit} crédit${currentCredit > 1 ? 's' : ''}`}</p>
+						<p className="text-sm">{`${pendingCredit} crédit${pendingCredit > 1 ? 's' : ''} en cours`}</p>
+					</div>
+				)}
 				<Separator orientation="vertical" className="w-[2px] h-[25px]" />
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button
-							onClick={() => signup()}
-							variant={'outline'}
-							className="text-blue-400">
-							Se connecter
-						</Button>
-					</DropdownMenuTrigger>
-					{/* <DropdownMenuTrigger asChild>
-						<Button variant={'outline'} className="text-blue-400">
-							Jimmy
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent>
-						<Link to={'/myspot'}>
-							<DropdownMenuItem>Mon Spot</DropdownMenuItem>
-						</Link>
-						<DropdownMenuItem>Se déconnecter</DropdownMenuItem>
-					</DropdownMenuContent> */}
-				</DropdownMenu>
+				{isAuthenticated ? (
+					<>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button variant={'default'}>{user.name}</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent className="flex flex-col gap-2 ">
+								<DropdownMenuItem asChild>
+									<Link to={'/myspot'}>Mon spot</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem asChild>
+									<Button variant={'destructive'} onClick={() => logout()}>
+										Se déconnecter
+									</Button>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</>
+				) : (
+					<Button
+						onClick={() => loginWithRedirect()}
+						variant={'outline'}
+						className="text-blue-400">
+						Se connecter
+					</Button>
+				)}
 			</div>
 		</div>
 	);
