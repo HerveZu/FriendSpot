d2a6062027decc5e55f11addaaf93e2efd253690
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-12-01 18:28:33 +0100

    Add landingConnect for authentification user

---
+++ frontend/src/components/landing-connect.tsx
@@ -0,0 +1,28 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { LogoLoader } from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { Footer } from './footer';
+
+export function LandingConnect() {
+	const { loginWithRedirect } = useAuth0();
+
+	return (
+		<div className="flex flex-col items-center w-full h-screen mt-48">
+			<div className="flex flex-col items-center gap-12 w-[80%]">
+				<LogoLoader loop={700} pause={800} />
+				<div className="flex flex-col items-center text-center gap-4 mt-5">
+					<h1 className="text-xl">
+						Bienvenue sur <span className="text-primary">FriendSpot</span> (bêta)
+					</h1>
+					<h2 className="text-lg">
+						Connectez-vous pour pouvoir utiliser l&apos;application !
+					</h2>
+				</div>
+				<Button className="w-full" onClick={() => loginWithRedirect()} variant={'default'}>
+					S&apos;authentifier
+				</Button>
+			</div>
+			<Footer />
+		</div>
+	);
+}
