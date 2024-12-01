0812008eb47f9f91772d660e19461742c836edf1
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-12-01 02:55:56 +0100

    Remove footer component

--- frontend/src/pages/root-layout-page.tsx
+++ frontend/src/pages/root-layout-page.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/header';
-import { Footer } from '@/components/footer';
-import { Separator } from '@/components/ui/separator';
 
 export function RootLayoutPage() {
 	return (
 		<>
 			<Header />
 			<Outlet />
-			<Separator orientation="horizontal" className='mt-14'/>
-			<Footer/>
 		</>
 	);
 }
