289155c2ee811b22d415ccde9675ab95ee6de09c
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-12-01 02:56:33 +0100

    Add loading with animate logo

--- frontend/src/pages/register-page.tsx
+++ frontend/src/pages/register-page.tsx
@@ -1,18 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useApiRequest } from '@/lib/hooks/use-api-request';
 import { useNavigate } from 'react-router-dom';
+import { LogoLoader } from '@/components/logo';
 
 export function RegisterPage() {
 	const navigate = useNavigate();
 	const { apiRequest } = useApiRequest();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		async function registerUser() {
-			await apiRequest('http://localhost:5001/@me/register', 'POST');
-			navigate('/');
+			setIsLoading(true);
+
+			try {
+				await apiRequest('http://localhost:5001/@me/register', 'POST');
+			} catch (error) {
+				console.log(error);
+			} finally {
+				navigate('/');
+				setIsLoading(false);
+			}
 		}
 		registerUser();
-	}, [apiRequest]);
+	}, [apiRequest, navigate]);
 
-	return <></>;
+	return (
+		<>
+			{isLoading && (
+				<div className="absolute left-[40%] top-[30%]">
+					<LogoLoader loop={700} pause={800} />
+				</div>
+			)}
+		</>
+	);
 }
