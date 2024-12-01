14d28b9a20e7c14361f27c7bbffe8453a28e3e64
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-12-01 18:29:50 +0100

    Modifing landing page with check if user is connected

--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -3,12 +3,9 @@ import { CustomPopUp } from '@/components/custom-pop-up';
 export function LandingPage() {
 	return (
 		<div className="flex flex-col mt-14 gap-5 text-lg px-8">
-			<h1 className="text-xl">
-				Bienvenue sur <span className="text-blue-400">FriendSpot</span> (bêta)
-			</h1>
-			<p className="text-base">
+			<h2 className="text-base">
 				12 places sont actuellement libres <br /> dans votre parking
-			</p>
+			</h2>
 			<div className="flex flex-col mt-5 gap-5">
 				<CustomPopUp
 					buttonTitle="Réserver une place"
@@ -29,10 +26,10 @@ export function LandingPage() {
 				</h2>
 				<div className="flex flex-col gap-1 border min-h-[50px] rounded-lg px-3 py-3">
 					<p className="text-base">Place no 123A</p>
-					<p className="text-xs">Jusqu'au 16 novembre à 20h</p>
+					<p className="text-xs">Jusqu&apos;au 16 novembre à 20h</p>
 				</div>
 				<div className="flex flex-col justify-center gap-1 border min-h-[50px] rounded-md px-2.5 py-2">
-					<p className="text-xs">Jusqu'au 16 novembre à 20h</p>
+					<p className="text-xs">Jusqu&apos;au 16 novembre à 20h</p>
 				</div>
 			</div>
 		</div>
--- frontend/src/pages/root-layout-page.tsx
+++ frontend/src/pages/root-layout-page.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/header';
+import { useAuth0 } from '@auth0/auth0-react';
+import { LandingConnect } from '@/components/landing-connect';
 
 export function RootLayoutPage() {
+	const { isAuthenticated } = useAuth0();
+
 	return (
-		<>
-			<Header />
-			<Outlet />
-		</>
+		<div className="w-full h-full">
+			{isAuthenticated ? (
+				<>
+					<Header />
+					<Outlet />
+				</>
+			) : (
+				<LandingConnect />
+			)}
+		</div>
 	);
 }
