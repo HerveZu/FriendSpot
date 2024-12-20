702232a9addb32cbde9ca349ef14dc053a645113
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-12-20 14:51:24 +0100

    Modifing text centering

--- frontend/src/components/action-button.tsx
+++ frontend/src/components/action-button.tsx
@@ -7,7 +7,7 @@ export const ActionButton = forwardRef<
 	ButtonProps & { info?: ReactNode; large?: boolean }
 >(({ large, className, ...props }, ref) => {
 	return (
-		<div className={'flex flex-col gap-2'}>
+		<div className={'flex flex-col gap-2 text-center'}>
 			{props.info && <span className={'text-sm text-primary'}>{props.info}</span>}
 			<Button ref={ref} size={'lg'} className={cn(large && 'h-14', className)} {...props} />
 		</div>
