dae3c01ea5ffa53b27e3a60cf2ecd034bf4a86a9
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2024-12-22 00:11:42 +0200

    Fix InlineAlert aligning

--- frontend/src/components/inline-alert.tsx
+++ frontend/src/components/inline-alert.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/lib/utils.ts';
 
 export function InlineAlert(props: { children: ReactNode; icon: ReactNode; className?: string }) {
 	return (
-		<Alert className={'border-warning bg-warning'}>
-			{props.icon}
-			<AlertTitle className={cn('ml-2 mt-1', props.className)}>{props.children}</AlertTitle>
+		<Alert className={'flex items-center gap-4'}>
+			<div>{props.icon}</div>
+			<AlertTitle className={cn(props.className)}>{props.children}</AlertTitle>
 		</Alert>
 	);
 }
