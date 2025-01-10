0a1027f3a14135bfa86fdadf1962a072f75ed9c2
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-01-11 00:53:25 +0100

    Init project mobile app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,14 +1,5 @@
-import { Stack } from 'expo-router';
-import { Auth0Provider } from 'react-native-auth0';
-import { UserProvider } from '@/app/components/user-provider';
-import { env } from '@/app/utils/env';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-	return (
-		<Auth0Provider domain={env.auth0.domain} clientId={env.auth0.clientId}>
-			<UserProvider>
-				<Stack />
-			</UserProvider>
-		</Auth0Provider>
-	);
+  return <Stack />;
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,17 @@
-import { View } from 'react-native';
+import { Text, View } from "react-native";
+import Signup from "./components/signup";
 
 export default function Index() {
-	return <View />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Signup />
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
 }
