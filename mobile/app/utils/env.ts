ca4cca3b106a0b4feb15e77169c6b8514a8fe470
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-01-21 17:46:23 +0100

    Initialize project

--- mobile/app/utils/env.ts
+++
@@ -1,8 +0,0 @@
-// see https://docs.expo.dev/guides/environment-variables/
-export const env = {
-	auth0: {
-		domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
-		clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID
-	},
-	api_url: process.env.EXPO_PUBLIC_API_URL
-};
