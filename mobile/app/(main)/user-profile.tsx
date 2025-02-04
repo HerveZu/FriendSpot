6c531529a3317fecf332206864eb6967f212e794
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-04 14:58:24 +0100

    style: Update global style

--- mobile/app/(main)/user-profile.tsx
+++ mobile/app/(main)/user-profile.tsx
@@ -22,8 +22,10 @@ import { ThemedIcon } from '~/components/ThemedIcon';
 import { TextInput } from '~/components/TextInput';
 import { Sheet, useSheetRef } from '~/components/nativewindui/Sheet';
 import avatar from '../../assets/avatar.png';
+import avatar2 from '../../assets/avatar-2.png';
 import { useApiRequest } from '~/endpoints/use-api-request';
 import { useDebounce } from 'use-debounce';
+import car from '../../assets/car-user-profil.png';
 
 interface Parking {
   id: string;
@@ -45,10 +47,10 @@ export default function UserProfileScreen() {
   const [selectedParking, setSelectedParking] = useState<Parking>();
   const [value] = useDebounce(search, 400);
 
-  useEffect(() => {
-    apiRequest<Parking[]>(`/parking?search=${value}`, 'GET').then(setParking);
-    console.log(parking);
-  }, [value]);
+  // useEffect(() => {
+  //   apiRequest<Parking[]>(`/parking?search=${value}`, 'GET').then(setParking);
+  //   console.log(parking);
+  // }, [value]);
 
   const saveParking = useCallback(async () => {
     try {
@@ -103,27 +105,29 @@ export default function UserProfileScreen() {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         keyboardVerticalOffset={100}>
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <ContentView className="mx-auto w-full rounded-lg p-4 shadow-lg ">
+          <ContentView className="mx-auto w-full rounded-lg p-4">
             <View className="flex-row items-center justify-around gap-5 ">
-              <View>
+              <View className="">
                 {userProfile.pictureUrl ? (
                   <Image
                     source={{ uri: userProfile.pictureUrl }}
-                    style={{ width: 100, height: 100, borderRadius: 50 }}
+                    style={{ width: 130, height: 130, borderRadius: 50 }}
                   />
                 ) : (
-                  <Image source={avatar} style={{ width: 130, height: 130, borderRadius: 65 }} />
+                  <View className="shadow-lg shadow-primary">
+                    <Image source={avatar} style={{ width: 130, height: 130, borderRadius: 65 }} />
+                  </View>
                 )}
               </View>
               <View className="items-center gap-4">
-                <Text variant={'largeTitle'} className="rounded-lg bg-primary px-2 text-white">
+                <Text variant={'title1'} className="rounded-lg bg-primary px-2 text-white">
                   {userProfile.displayName}
                 </Text>
                 <Rating rating={userProfile.rating} stars={3} color={colors.primary} />
               </View>
             </View>
             <TextInput
-              className="mt-10 h-auto w-full justify-center rounded-lg bg-primary p-2 text-xl"
+              className="my-auto mt-10 h-auto w-full justify-center rounded-lg bg-primary text-lg"
               style={{ color: 'white' }}
               value={currentEmail}
               editable={true}
@@ -134,41 +138,53 @@ export default function UserProfileScreen() {
               }}
               onEndEditing={(event) => verifyEmail(event.nativeEvent.text)}
             />
-            <View className="mt-10 flex-row items-center justify-center">
+            <View className="mt-8 flex-row items-center justify-center">
               <View className="h-px flex-1 bg-primary" />
               <Text className="mx-4 text-primary">Mon spot</Text>
               <View className="h-px flex-1 bg-primary" />
             </View>
             <Button
-              className="mt-10 h-auto flex-col items-start justify-start rounded-lg bg-primary p-2"
+              className="mt-8 h-auto flex-col items-start rounded-lg bg-primary"
               onPress={() => setBookSheetOpen(true)}>
-              <Text className="text-xl text-white">
-                {userProfile.spot
-                  ? userProfile.spot.parking.name
-                  : 'Aucun nom de parking de défini'}
-              </Text>
-              <View className="w-11/12 flex-row items-center justify-start gap-2 py-1">
+              <View className="w-full flex-row items-center justify-between px-1">
+                <Text className="text-lg text-white">
+                  {userProfile.spot
+                    ? userProfile.spot.parking.name
+                    : 'Aucun nom de parking de défini'}
+                </Text>
+                <ThemedIcon name={'pencil'} size={18} color={'white'} />
+              </View>
+              <View className="w-11/12 flex-row items-center justify-start gap-2">
                 <ThemedIcon name={'map-marker'} size={20} color={'white'} />
                 <View>
-                  <Text className="text-md text-white ">
+                  <Text className="text-sm text-white ">
                     {userProfile.spot?.parking
                       ? userProfile.spot?.parking?.address
                       : 'Aucune adresse parking définie'}
                   </Text>
-                  {/* Ici */}
                 </View>
               </View>
             </Button>
-            <View className=" mt-6 flex-row justify-between rounded-lg px-4 py-2">
-              <View className="mb-2 items-center gap-3">
-                <Text className="max-w-32 rounded-lg">
-                  Utilisé par {userProfile.spot?.currentlyUsedBy?.displayName ?? 'hervé'}
-                </Text>
-                <View className="h-40 w-32 rounded-lg border-2 border-dashed border-primary"></View>
+            <View className=" mt-8 flex-row justify-center gap-4 rounded-lg border-2 border-primary px-2 py-2">
+              <View className="mb-2 flex-1 items-center gap-5 ">
+                <View className="flex-row items-center gap-2 ">
+                  <Text className="max-w-32 text-ellipsis rounded-lg text-sm">
+                    Utilisé par {'\n'}{' '}
+                    <Text className="text-primary">
+                      {userProfile.spot?.currentlyUsedBy?.displayName ?? 'Jean-marc'}
+                    </Text>
+                  </Text>
+                  <Image source={avatar2} style={{ width: 25, height: 25, borderRadius: 65 }} />
+                </View>
+                {/* <View className="h-40 w-32 rounded-lg border-2 border-dashed border-primary"></View> */}
+                <View className="justify-centert h-32 w-40">
+                  <Image className="h-full w-full rotate-90 " source={car} alt="car" />
+                </View>
               </View>
-              <View className="mt-5 flex gap-2">
+              <View className="my-auto h-44 rounded-lg border-2 border-white"></View>
+              <View className="mt-5 flex-1 items-center gap-2">
                 <Text className="text-xl">Place n°</Text>
-                <Button className="h-auto rounded-lg bg-primary p-2">
+                <Button className="h-auto w-[50%] rounded-lg bg-primary p-2">
                   <Text className="text-xl text-white">
                     {userProfile.spot ? userProfile.spot.name : 'A43'}
                   </Text>
@@ -186,12 +202,12 @@ export default function UserProfileScreen() {
           snapPoints={[550]}>
           <BottomSheetView>
             <SafeAreaView>
-              <ContentView className="rounded-lg p-4 shadow-lg">
+              <ContentView className="rounded-lg p-4">
                 <View className="h-full flex-1 flex-col gap-8 pb-8 ">
-                  <View className="mx-auto w-11/12 grow flex-col gap-6">
-                    <View className="flex-row items-center gap-4 rounded-lg border border-foreground p-2">
+                  <View className="mx-auto grow flex-col gap-6">
+                    <View className="flex-row items-center gap-4 rounded-lg border border-primary p-2">
                       <TextInput
-                        className="flex w-full justify-center rounded-lg border border-foreground p-2"
+                        className="flex w-full justify-center rounded-lg p-2 text-base"
                         editable={true}
                         value={search}
                         onChangeText={(text) => setSearch(text)}
@@ -199,7 +215,7 @@ export default function UserProfileScreen() {
                         placeholder="Rechercher un parking"
                       />
                     </View>
-                    <View className="mx-auto mt-2 h-full gap-8">
+                    <View className="relative mt-2 h-full gap-8">
                       {parking &&
                         parking.slice(0, 2).map((parking) => (
                           <Button
@@ -212,13 +228,13 @@ export default function UserProfileScreen() {
                             <Text className="text-white">32 spots</Text>
                           </Button>
                         ))}
-                      <Button
-                        className="mt-20 rounded-lg bg-primary p-2"
-                        disabled={!selectedParking}
-                        onPress={() => saveParking()}>
-                        <Text className="py-2 text-white">Enregistrer</Text>
-                      </Button>
                     </View>
+                    <Button
+                      className="absolute bottom-24 mt-auto w-full rounded-lg bg-primary p-2"
+                      disabled={!selectedParking}
+                      onPress={() => saveParking()}>
+                      <Text className="py-2 text-white">Enregistrer</Text>
+                    </Button>
                   </View>
                 </View>
               </ContentView>
