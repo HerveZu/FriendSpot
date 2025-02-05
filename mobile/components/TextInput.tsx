4a80463d11e21121c97a8547756b28f884875321
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-05 12:45:55 +0100

    Style: Add posibility to add icon on textInput

--- mobile/components/TextInput.tsx
+++ mobile/components/TextInput.tsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { TextInput as ReactTextInput, TextInputProps } from 'react-native';
+import { TextInput as ReactTextInput, TextInputProps, View } from 'react-native';
+import { ThemedIcon } from '~/components/ThemedIcon';
 
 import { cn } from '~/lib/cn';
 import { useColorScheme } from '~/lib/useColorScheme';
 import { omitObj } from '~/lib/utils';
 
-export function TextInput({ className, onFocus, onBlur, style, ...props }: TextInputProps) {
+interface CustomTextInputProps extends TextInputProps {
+  className?: string;
+  icon?: string;
+  iconPosition?: 'left' | 'right';
+  iconSize?: number;
+}
+
+export function TextInput({
+  className,
+  onFocus,
+  onBlur,
+  icon,
+  style,
+  iconPosition = 'right',
+  iconSize = 20,
+  ...props
+}: CustomTextInputProps) {
   const [focus, setFocus] = useState(false);
   const { colors } = useColorScheme();
   const inputRef = React.createRef<ReactTextInput>();
@@ -15,27 +32,35 @@ export function TextInput({ className, onFocus, onBlur, style, ...props }: TextI
   }, [focus, inputRef.current]);
 
   return (
-    <ReactTextInput
-      ref={inputRef}
-      onFocus={(e) => {
-        setFocus(true);
-        onFocus && onFocus(e);
-      }}
-      onBlur={(e) => {
-        setFocus(false);
-        onBlur && onBlur(e);
-      }}
-      value={props.value}
-      className={cn('rounded-lg p-4 text-xl', className)}
-      placeholderTextColor={colors.grey}
-      style={[
-        {
-          color: colors.foreground,
-          borderColor: focus ? colors.primary : colors.card,
-        },
-        omitObj(style),
-      ]}
-      {...props}
-    />
+    <View className={cn('relative w-full flex-row items-center gap-4', className)}>
+      {icon && iconPosition === 'left' && (
+        <ThemedIcon name={icon} size={iconSize} color={colors.primary} />
+      )}
+      <ReactTextInput
+        ref={inputRef}
+        onFocus={(e) => {
+          setFocus(true);
+          onFocus && onFocus(e);
+        }}
+        onBlur={(e) => {
+          setFocus(false);
+          onBlur && onBlur(e);
+        }}
+        value={props.value}
+        placeholderTextColor={colors.grey}
+        style={[
+          {
+            color: colors.foreground,
+            borderColor: focus ? colors.primary : colors.card,
+          },
+          omitObj(style),
+        ]}
+        {...props}
+        className="flex-1"
+      />
+      {icon && iconPosition === 'right' && (
+        <ThemedIcon name={icon} size={iconSize} color={colors.primary} />
+      )}
+    </View>
   );
 }
