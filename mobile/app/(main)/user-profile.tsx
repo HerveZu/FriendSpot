3cc7381f9c184fda4316dad47077d7007e57dbbc
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-16 00:35:37 +0100

    feat: Update for displaying user image and refoctor code

--- mobile/app/(main)/user-profile.tsx
+++ mobile/app/(main)/user-profile.tsx
@@ -21,17 +21,15 @@ import { Button } from '~/components/nativewindui/Button';
 import { ThemedIcon } from '~/components/ThemedIcon';
 import { TextInput } from '~/components/TextInput';
 import { Sheet, useSheetRef } from '~/components/nativewindui/Sheet';
-import avatar2 from '../../assets/avatar-2.png';
 import { useApiRequest } from '~/endpoints/use-api-request';
 import { useDebounce } from 'use-debounce';
-import { MeAvatar } from '~/components/UserAvatar';
-import car from '../../assets/car-user-profil.png';
+import { MeAvatar, UserAvatar } from '~/components/UserAvatar';
+import car from '../../assets/car-user-profile.png';
 import * as ImagePicker from 'expo-image-picker';
 import { LogoCard } from '~/components/Logo';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { isToday, isTomorrow, format, differenceInMinutes, minutesToHours } from 'date-fns';
-import mime from 'mime';
-
+import { useActualTime } from '~/lib/useActualTime';
+import { differenceInMinutes, minutesToHours, formatRelative } from 'date-fns';
+import { useUploadUserPicture } from '~/endpoints/upload-user-picture';
 interface Parking {
   id: string;
   address: string;
@@ -47,18 +45,22 @@ export default function UserProfileScreen() {
   const [parking, setParking] = useState<Parking[]>();
   const [search, setSearch] = useState('');
   const [selectedParking, setSelectedParking] = useState<Parking>();
-  const [value] = useDebounce(search, 400);
   const { userProfile, updateInternalProfile, refreshProfile } = useCurrentUser();
   const [currentDisplayName, setCurrentDisplayName] = useState<string>(userProfile.displayName);
   const [oldDisplayName, setOldDisplayName] = useState<string>(userProfile.displayName);
   const bottomSheetModalRef = useSheetRef();
   const [currentSpotName, setCurrentSpotName] = useState<string>(userProfile.spot?.name || '');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchDebounce] = useDebounce(search, 200);
 
-  const available = userProfile?.spot?.available;
+  const available = userProfile?.spot?.currentlyAvailable;
   const usingUntil = userProfile?.spot?.currentlyUsedBy?.usingUntil;
-  const nextUse = userProfile.spot?.nextUse;
   const currentlyUsedBy = userProfile.spot?.currentlyUsedBy;
+
+  const spotUsedBeMe = !available && !currentlyUsedBy;
+
+  const now = useActualTime(30_000);
+  const uploadPicture = useUploadUserPicture();
 
   const checkIfPlural = (value: number, text: string) => {
     if (value > 1) {
@@ -68,20 +70,6 @@ export default function UserProfileScreen() {
     }
   };
 
-  const formatDate = (time: Date | null | undefined) => {
-    if (time) {
-      const date = new Date(time);
-      if (isToday(date)) {
-        return `aujourd'hui à ${format(date, 'HH:mm')}`;
-      } else if (isTomorrow(date)) {
-        return `demain à ${format(date, 'HH:mm')}`;
-      } else {
-        return format(date, 'dd/MM/yyyy à HH:mm');
-      }
-    }
-    return '';
-  };
-
   const handleLogout = () => {
     const auth = getAuth();
     signOut(auth).catch((error) => {
@@ -89,38 +77,29 @@ export default function UserProfileScreen() {
       console.error('Error signing out: ', error);
     });
   };
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
       quality: 1,
     });
-    if (!result.canceled && result.assets && result.assets.length > 0) {
-      const storage = getStorage();
-      const imageUri = result.assets[0].uri;
-      const response = await fetch(imageUri);
-      console.log(response);
-      const blob = await response.blob();
-
-      const extension = mime.getExtension(result.assets[0].type || '') || 'jpg';
-
-      const userId = user?.uid;
-
-      const photoUrlRef = ref(storage, `images/${userId}.${extension}`);
-
-      await uploadBytes(photoUrlRef, blob);
-
-      getDownloadURL(ref(photoUrlRef)).then((url) => {
-        updateInternalProfile(url, userProfile.displayName);
-      });
-    } else {
+    if (result.canceled) {
       return;
     }
+    const response = await fetch(result.assets[0].uri);
+    const imageBody = await response.blob();
+    const { writeUrl, readonlyUrl } = await uploadPicture();
+    await fetch(writeUrl, {
+      method: 'PUT',
+      body: imageBody,
+    });
+    await updateInternalProfile(`${readonlyUrl}#_n=${Math.random()}`, userProfile.displayName);
   };
 
   useEffect(() => {
-    apiRequest<Parking[]>(`/parking?search=${value}`, 'GET').then(setParking);
-  }, [value]);
+    apiRequest<Parking[]>(`/parking?search=${searchDebounce}`, 'GET').then(setParking);
+  }, [searchDebounce]);
 
   useEffect(() => {
     if (bottomSheet) {
@@ -157,9 +136,16 @@ export default function UserProfileScreen() {
   }
 
   const DisplayCar = () => {
-    if (!available || currentlyUsedBy) {
+    if (spotUsedBeMe || !!currentlyUsedBy) {
       return (
-        <View className="h-28 w-[105px] flex-1">
+        <View className="relative h-28 w-[105px] flex-1">
+          {!!currentlyUsedBy && (
+            <UserAvatar
+              displayName={userProfile.spot.currentlyUsedBy.displayName}
+              pictureUrl={userProfile.spot.currentlyUsedBy.pictureUrl}
+              className="absolute left-[-5] top-[-5] z-10 h-8 w-8"
+            />
+          )}
           <Image
             className="h-full w-full"
             source={car}
@@ -176,6 +162,9 @@ export default function UserProfileScreen() {
   };
 
   const SpotUsedBy = () => {
+    if (!userProfile.spot) {
+      return;
+    }
     const getRemainingTime = (endTime: Date | null | undefined) => {
       if (endTime) {
         const now = new Date();
@@ -196,22 +185,11 @@ export default function UserProfileScreen() {
       }
       return '';
     };
-    if (currentlyUsedBy) {
+    if (!!currentlyUsedBy) {
       return (
         <View className="w-full flex-1 flex-col justify-center gap-6">
           <Text className="text-center text-base font-bold">En cours d'utilisation</Text>
-          <View className="flex flex-col gap-4">
-            <View className="flex-row items-center justify-center gap-2 ">
-              <Image
-                className="h-6 w-6"
-                // source={{ uri: userProfile.spot.currentlyUsedBy.pictureUrl }}
-                source={avatar2}
-              />
-              <Text className="flex justify-center text-base">
-                {userProfile.spot.currentlyUsedBy.displayName}
-              </Text>
-            </View>
-          </View>
+          <Text className="text-center">Par {currentlyUsedBy.displayName}</Text>
 
           <View className="mt-2 flex-row items-center justify-center gap-2 rounded-lg bg-primary px-4 py-2">
             <ThemedIcon name={'clock-o'} size={18} color={'white'} />
@@ -226,15 +204,11 @@ export default function UserProfileScreen() {
     } else {
       return (
         <View className="w-full flex-1 flex-col justify-center gap-6">
-          <Text className="text-center text-lg font-bold">{`${!available ? 'Votre spot est libre' : 'Vous occupez votre place'}`}</Text>
-
+          <Text className="text-center text-lg font-bold">{`${available ? 'Ton spot est libre' : 'Tu occupes ta place'}`}</Text>
           {userProfile.spot?.nextUse && (
-            <View className="mt-2 flex-row items-center justify-center gap-2 rounded-lg bg-primary px-4 py-2">
-              <ThemedIcon name={'clock-o'} size={18} color={'white'} />
-              <Text className="text-center text-sm text-white">
-                {`Prochaine utilisation ${formatDate(nextUse)}`}
-              </Text>
-            </View>
+            <Text className="text-center">
+              Jusqu'à {formatRelative(userProfile.spot.nextUse, now)}
+            </Text>
           )}
         </View>
       );
@@ -246,7 +220,7 @@ export default function UserProfileScreen() {
       <KeyboardAvoidingView
         style={{ flex: 1 }}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={100}>
+        keyboardVerticalOffset={65}>
         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
           <ContentView className="bg-gray mx-auto w-full rounded-lg p-4">
             <View className="flex w-full flex-row items-center gap-6">
@@ -340,7 +314,7 @@ export default function UserProfileScreen() {
           <BottomSheetView>
             <ContentSheetView className="h-full flex-col justify-between rounded-lg">
               <View className="gap-6">
-                <View className="flex-col items-center gap-4 rounded-lg border border-foreground">
+                <View className="flex-col items-center gap-4 rounded-lg border border-border bg-background">
                   <TextInput
                     className="flex w-full justify-center rounded-lg p-4 text-base"
                     icon={'search'}
@@ -363,7 +337,7 @@ export default function UserProfileScreen() {
                             setSearch(parking.address),
                             setCurrentSpotName('');
                         }}
-                        className="min-h-16 min-w-60 flex-row items-center justify-between rounded-lg border border-foreground bg-card p-4">
+                        className="min-h-16 min-w-60 flex-row items-center justify-between rounded-lg border border-border bg-card p-4">
                         <Text className="max-w-52">{parking.address}</Text>
                         <Text className="">{`${parking.spotsCount} ${parking.spotsCount > 1 ? 'spots' : 'spot'}`}</Text>
                       </Pressable>
@@ -374,7 +348,7 @@ export default function UserProfileScreen() {
                 <View className="w-full flex-row items-center">
                   <Text className="flex-1 text-lg">N° de place</Text>
                   <TextInput
-                    className="flex-1 rounded-lg border border-foreground p-4"
+                    className="flex-1 rounded-lg border border-border bg-background p-4"
                     icon={'pencil'}
                     iconPosition="right"
                     iconSize={18}
--- mobile/components/UserAvatar.tsx
+++ mobile/components/UserAvatar.tsx
@@ -1,16 +1,17 @@
-import { ViewProps } from 'react-native';
+import React from 'react';
+import { View, ViewProps } from 'react-native';
+
 import { useCurrentUser } from '~/authentication/UserProvider';
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/nativewindui/Avatar';
 import { Text } from '~/components/nativewindui/Text';
+import { cn } from '~/lib/cn';
 
-export function UserAvatar({
-  displayName,
-  pictureUrl,
-  ...props
-}: {
+type UserAvatarProps = {
   displayName: string;
-  pictureUrl?: string;
-} & ViewProps) {
+  pictureUrl?: string | null;
+} & ViewProps;
+
+export function UserAvatar({ displayName, pictureUrl, ...props }: UserAvatarProps) {
   const namesInitials = displayName.split(' ').map((name) => name[0]?.toUpperCase());
   const userInitials = [
     namesInitials[0],
@@ -19,15 +20,15 @@ export function UserAvatar({
 
   return (
     <Avatar alt="Profile" {...props}>
-      <AvatarImage source={{ uri: pictureUrl }} />
-      <AvatarFallback className="relative">
+      <AvatarImage source={{ uri: pictureUrl ?? undefined }} />
+      <AvatarFallback>
         <Text>{userInitials.join('')}</Text>
       </AvatarFallback>
     </Avatar>
   );
 }
 
-export function MeAvatar({ ...props }: ViewProps) {
+export function MeAvatar(props: ViewProps) {
   const { userProfile } = useCurrentUser();
 
   return (
@@ -36,5 +37,14 @@ export function MeAvatar({ ...props }: ViewProps) {
       pictureUrl={userProfile.pictureUrl}
       {...props}
     />
+  );
+}
+
+export function User({ className, ...props }: UserAvatarProps) {
+  return (
+    <View className="flex-row items-center gap-4">
+      <UserAvatar className={cn('h-9 w-9', className)} {...props} />
+      <Text className="font-medium">{props.displayName}</Text>
+    </View>
   );
 }
