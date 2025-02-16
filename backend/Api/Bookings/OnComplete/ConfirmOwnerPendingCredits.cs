58f82765cfd2d3ea3199f5898d4c1b7e0064e0d2
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-02-16 19:04:22 +0100

    Make confirm pending credits action more explicit

--- backend/Api/Bookings/OnComplete/ConfirmOwnerPendingCredits.cs
+++ backend/Api/Bookings/OnComplete/ConfirmOwnerPendingCredits.cs
@@ -6,26 +6,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Bookings.OnComplete;
 
-internal sealed class TransferPendingCreditsToWallet(
-    ILogger<TransferPendingCreditsToWallet> logger,
+internal sealed class ConfirmOwnerPendingCredits(
+    ILogger<ConfirmOwnerPendingCredits> logger,
     AppDbContext dbContext
 ) : IDomainEventHandler<ParkingSpotBookingCompleted>
 {
     public async Task Handle(ParkingSpotBookingCompleted notification, CancellationToken cancellationToken)
     {
         logger.LogInformation(
-            "Transfer pending credits to user {UserId} after booking marked complete",
+            "Confirming owner {OwnerId} pending credits after booking marked complete",
             notification.OwnerId);
 
         var transactionReference = notification.BookingId.ToString();
 
-        var userWallet = await dbContext
+        var ownerWallet = await dbContext
             .Set<Wallet>()
             .FirstAsync(wallet => wallet.UserId == notification.OwnerId, cancellationToken);
 
-        userWallet.ConfirmPending(transactionReference);
+        ownerWallet.ConfirmPending(transactionReference);
 
-        dbContext.Set<Wallet>().Update(userWallet);
+        dbContext.Set<Wallet>().Update(ownerWallet);
         await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
