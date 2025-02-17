0a0e4a2dd42a568916b036169225ec5aae688bb9
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-02-17 21:33:05 +0100

    Fix .eslintrc.js config

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,31 +1,7 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
+    'prettier/prettier': 'error',
   },
 };
-
-// Don't delete
