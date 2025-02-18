674deb9ffe8b9394f7afc7f7506785c8d3ddff07
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-18 23:25:20 +0100

    fix: Change config prettier and eslint

--- mobile/prettier.config.js
+++ mobile/prettier.config.js
@@ -4,6 +4,7 @@ module.exports = {
   singleQuote: true,
   bracketSameLine: true,
   trailingComma: 'es5',
+  endOfLine: 'lf',
 
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
   tailwindAttributes: ['className'],
