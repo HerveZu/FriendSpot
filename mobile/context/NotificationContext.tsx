5a2ff4ea2c71f3faf78d027acc98a63899dd9945
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-20 23:33:06 +0100

    refacto: Change type

--- mobile/context/NotificationContext.tsx
+++ mobile/context/NotificationContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect, useRef, ReactNode } from 'react';
 import * as Notifications from 'expo-notifications';
-import { Subscription } from 'expo-modules-core';
+import { EventSubscription } from 'expo-modules-core';
 import { registerForPushNotificationsAsync } from '../utils/registerForPushNotificationsAsync';
 
 interface NotificationContextType {
@@ -28,8 +28,8 @@ export const NotificationProvider: React.FC<NotificationProviderProps> = ({ chil
   const [notification, setNotification] = useState<Notifications.Notification | null>(null);
   const [error, setError] = useState<Error | null>(null);
 
-  const notificationListener = useRef<Subscription>();
-  const responseListener = useRef<Subscription>();
+  const notificationListener = useRef<EventSubscription>();
+  const responseListener = useRef<EventSubscription>();
 
   useEffect(() => {
     registerForPushNotificationsAsync().then(
