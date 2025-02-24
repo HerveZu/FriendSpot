44b6b03d34f5350f36eaaac3a15b845227dbaf32
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-24 14:47:30 +0100

    Refacto+feat: Remove unused code and update function for displaying notification

--- mobile/context/NotificationContext.tsx
+++ mobile/context/NotificationContext.tsx
@@ -3,6 +3,7 @@ import * as Notifications from 'expo-notifications';
 import { EventSubscription } from 'expo-modules-core';
 import { registerForPushNotificationsAsync } from '../utils/registerForPushNotificationsAsync';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { Linking } from 'react-native';
 
 interface NotificationContextType {
   expoPushToken: string | null;
@@ -28,7 +29,6 @@ export const NotificationProvider: React.FC<NotificationProviderProps> = ({ chil
   const [expoPushToken, setExpoPushToken] = useState<string | null>(null);
   const [notification, setNotification] = useState<Notifications.Notification | null>(null);
   const [error, setError] = useState<Error | null>(null);
-  const [websocketMessage, setWebsocketMessage] = useState<MessageEvent<any> | null>();
 
   const notificationListener = useRef<EventSubscription>();
   const responseListener = useRef<EventSubscription>();
@@ -39,27 +39,29 @@ export const NotificationProvider: React.FC<NotificationProviderProps> = ({ chil
       (error) => setError(error)
     );
 
-    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
-      console.log('🔔 Notification Received: ', notification);
-      setNotification(notification);
-    });
+    // Add a send POST with expoPushToken at backend
 
-    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log(
-        '🔔 Notification Response: ',
-        JSON.stringify(response, null, 2),
-        JSON.stringify(response.notification.request.content.data, null, 2)
-      );
-    });
+    // notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+    //   console.log('🔔 Notification Received: ', notification);
+    //   setNotification(notification);
+    // });
 
-    return () => {
-      if (notificationListener.current) {
-        Notifications.removeNotificationSubscription(notificationListener.current);
-      }
-      if (responseListener.current) {
-        Notifications.removeNotificationSubscription(responseListener.current);
-      }
-    };
+    // responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+    //   console.log(
+    //     '🔔 Notification Response: ',
+    //     JSON.stringify(response, null, 2),
+    //     JSON.stringify(response.notification.request.content.data, null, 2)
+    //   );
+    // });
+
+    // return () => {
+    //   if (notificationListener.current) {
+    //     Notifications.removeNotificationSubscription(notificationListener.current);
+    //   }
+    //   if (responseListener.current) {
+    //     Notifications.removeNotificationSubscription(responseListener.current);
+    //   }
+    // };
   }, []);
 
   const displayNotification = async (title: string, body: string, data: object) => {
@@ -79,9 +81,8 @@ export const NotificationProvider: React.FC<NotificationProviderProps> = ({ chil
 
   useEffect(() => {
     if (lastMessage !== null) {
-      setWebsocketMessage(lastMessage);
-
-      // displayNotification()
+      const messageData = JSON.parse(lastMessage.data);
+      displayNotification(messageData.title, messageData.body, messageData.data);
     }
   }, [lastMessage]);
 
