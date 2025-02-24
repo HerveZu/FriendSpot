82c184bd412777bb4e8283efa2d476e701a7f632
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-02-24 16:13:08 +0100

    feat: Adding Icon for android, ios and spash screen

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,9 +1,42 @@
 module.exports = {
-  extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react-hooks/exhaustive-deps': 'off',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  ignorePatterns: 'components/nativewindui/**',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-unresolved': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
