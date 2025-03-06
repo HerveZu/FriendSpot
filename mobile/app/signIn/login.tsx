90174d3f1582494850224ed2e1adfde43d7b9a4e
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-03-06 23:29:52 +0100

    feat: Setup and install package for display svg

--- mobile/app/signIn/login.tsx
+++ mobile/app/signIn/login.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
+import { SafeAreaView, View, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useRouter } from 'expo-router';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
-import { SafeAreaView } from 'react-native';
 import { isEmail } from 'validator';
-
 import { AuthForm, AuthFormInput, AuthFormTitle } from '~/authentication/AuthForm';
 import { notEmpty } from '~/lib/utils';
+import Image1 from '~/assets/image1.svg';
 
 export default function LoginScreen() {
   const [email, setEmail] = useState<string>();
@@ -15,47 +16,58 @@ export default function LoginScreen() {
   const router = useRouter();
 
   return (
-    <SafeAreaView>
-      <AuthForm
-        title={<AuthFormTitle title="Se connecter" />}
-        error={error}
-        onSubmit={() =>
-          signInWithEmailAndPassword(auth, email!, password!)
-            .then(() => router.navigate('/home'))
-            .catch(() => {
-              setError('Email ou mot de passe incorrect');
-            })
-        }
-        submitText="Se connecter">
-        <AuthFormInput
-          value={email}
-          onValueChange={setEmail}
-          placeholder="Adresse email"
-          inputMode="email"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          validators={[
-            {
-              validate: (email) => !email || isEmail(email),
-              message: "L'adresse e-mail n'est pas valide",
-            },
-            {
-              validate: notEmpty,
-            },
-          ]}
-        />
-        <AuthFormInput
-          value={password}
-          onValueChange={setPassword}
-          placeholder="Mot de passe"
-          secure
-          validators={[
-            {
-              validate: notEmpty,
-            },
-          ]}
-        />
-      </AuthForm>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <AuthForm
+              title={<AuthFormTitle title="Se connecter" />}
+              error={error}
+              onSubmit={() =>
+                signInWithEmailAndPassword(auth, email!, password!)
+                  .then(() => router.navigate('/home'))
+                  .catch(() => {
+                    setError('Email ou mot de passe incorrect');
+                  })
+              }
+              submitText="Se connecter"
+            >
+              <View className='items-center relative top-[-50px]'>
+                <Image1 width={300} height={300} />
+              </View>
+                <AuthFormInput
+                  value={email}
+                  onValueChange={setEmail}
+                  placeholder="Adresse email"
+                  inputMode="email"
+                  autoCapitalize="none"
+                  keyboardType="email-address"
+                  validators={[
+                    {
+                      validate: (email) => !email || isEmail(email),
+                      message: "L'adresse e-mail n'est pas valide",
+                    },
+                    {
+                      validate: notEmpty,
+                    },
+                  ]}
+                />
+                <AuthFormInput
+                  value={password}
+                  onValueChange={setPassword}
+                  placeholder="Mot de passe"
+                  secure
+                  validators={[
+                    {
+                      validate: notEmpty,
+                    },
+                  ]}
+                />
+            </AuthForm>
+          </SafeAreaView>
+        </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,9 +1,18 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
 
 module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
