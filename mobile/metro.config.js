9d235481cb4ddff9685ceb57334449c8dfe5537c
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-03-09 01:15:58 +0100

    fix: Update metro config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,18 +1,23 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const createConfig = () => {
+  const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+  return config;
 };
 
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter(ext => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
+const config = createConfig();
 
 module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
