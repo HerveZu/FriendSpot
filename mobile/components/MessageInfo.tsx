a77724e81376fcdafba148b328a7a2c99d8c4993
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-03-16 15:59:24 +0100

    feat: Change the display if props.action is on true

--- mobile/components/MessageInfo.tsx
+++ mobile/components/MessageInfo.tsx
@@ -1,13 +1,25 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
+import { Button } from "./nativewindui/Button";
 import { ThemedIcon } from "./ThemedIcon";
 import { useColorScheme } from '~/lib/useColorScheme';
 
-export function MessageInfo(props: { info: string}) {
+import { View } from "react-native";
+
+export function MessageInfo(props: { info: string, action?: () => void }) {
     const { colors } = useColorScheme();
     return (
-        <View className="flex-row mx-auto items-center w-full gap-4" >
-            <ThemedIcon name="lightbulb-o" size={22} color={colors.primary} />
-            <Text className="shrink text-foreground text-base font-semibold">{props.info}</Text>
-        </View>
+        <>
+            {props.action ? (
+                <Button className="flex-row mx-auto justify-center items-center w-full gap-4" variant="tonal" onPress={() => props.action && props.action()}>
+                    <ThemedIcon name="lightbulb-o" size={22} color={colors.primary} />
+                    <Text className="shrink text-foreground text-base font-semibold">{props.info}</Text>
+                </Button>
+            ) : (
+                <View className="flex-row items-center w-full gap-4">
+                    <ThemedIcon name="lightbulb-o" size={22} color={colors.primary} />
+                    <Text className="shrink text-foreground text-base font-semibold">{props.info}</Text>
+                </View>
+            )}
+        </>
     );
 }
