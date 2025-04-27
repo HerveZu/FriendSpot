d541a32c0f6ea1a5d6e7a4c6f1b5054d042317e5
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-04-27 12:09:27 +0200

    Add boundary tests for UserRating modification methods

--- backend/Domain.Tests/Users/UserRatingTests.cs
+++ backend/Domain.Tests/Users/UserRatingTests.cs
@@ -15,10 +15,42 @@ public sealed class UserRatingTests
         // Act
         for (var i = 0; i < 9999; i++)
         {
+            userRating.NeutralIncrease();
+        }
+
+        // Assert
+        Assert.That(userRating.Rating, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void GoodIncrease_ShouldNotExceedMaxStar()
+    {
+        // Arrange
+        var userRating = UserRating.Neutral();
+
+        // Act
+        for (var i = 0; i < 9999; i++)
+        {
             userRating.GoodIncrease();
         }
 
         // Assert
         Assert.That(userRating.Rating, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void BadDecrease_ShouldNotRateUnderZero()
+    {
+        // Arrange
+        var userRating = UserRating.Neutral();
+
+        // Act
+        for (var i = 0; i < 9999; i++)
+        {
+            userRating.BadDecrease();
+        }
+
+        // Assert
+        Assert.That(userRating.Rating, Is.EqualTo(0));
     }
 }
