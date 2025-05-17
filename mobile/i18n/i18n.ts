b55913b567ed1bb4f24bd533a53c9a53ab1f765a
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-05-18 01:18:01 +0200

    Properly install i18n

--- mobile/i18n/i18n.ts
+++ mobile/i18n/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { getLocales } from 'expo-localization';
 import en from './en.json';
 import fr from './fr.json';
 
@@ -12,10 +13,15 @@ const resources = {
   },
 };
 
+const deviceLanguage = getLocales()[0].languageCode;
+
 i18n.use(initReactI18next).init({
   resources,
+  lng: deviceLanguage ?? undefined,
   fallbackLng: 'en',
   interpolation: {
-    escapeValue: false,
+    escapeValue: false, // not needed
   },
 });
+
+export default i18n;
