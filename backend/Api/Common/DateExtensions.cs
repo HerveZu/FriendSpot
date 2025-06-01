c9262bf49ee79748bed23b6cda7105deb86c78c1
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2025-06-01 11:09:42 +0200

    Remove unused DateExtensions.cs

--- backend/Api/Common/DateExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Common;
-
-internal static class DateExtensions
-{
-    public static DateTimeOffset Max(DateTimeOffset a, DateTimeOffset b)
-    {
-        return a > b ? a : b;
-    }
-}
