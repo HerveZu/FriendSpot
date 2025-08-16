01e484fbbe9d07bbc8739d39cf8fa65bcbf0c66c
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-08-16 08:27:48 +0200

    fix: Bottom provider move to a higher file

--- mobile/app/(authenticated)/(main)/_layout.tsx
+++ mobile/app/(authenticated)/(main)/_layout.tsx
@@ -1,5 +1,4 @@
 import { FontAwesome6 } from '@expo/vector-icons';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Tabs } from 'expo-router';
 import { useCurrentUser } from '~/authentication/UserProvider';
 import { ThemedIcon, ThemedIconProps } from '~/components/ThemedIcon';
@@ -16,80 +15,77 @@ export default function MainLayout() {
 
   return (
     <>
-      {/*BottomSheetModalProvider children need to have access to the currentUser*/}
-      <BottomSheetModalProvider>
-        {/*<AskUserToRate>*/}
-        <Tabs
-          initialRouteName="my-spot"
-          screenOptions={{
-            headerShown: false,
-            tabBarShowLabel: false,
-            sceneStyle: { backgroundColor: 'transparent' },
-            tabBarStyle: {
-              paddingTop: 5,
-              backgroundColor: 'transparent',
-            },
-          }}>
-          <Tabs.Screen
-            name="my-spot"
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <TabIcon
-                  name="house"
-                  component={FontAwesome6}
-                  size={22}
-                  focused={focused}
-                  info={t('tabs.mySpot')}
+      {/*<AskUserToRate>*/}
+      <Tabs
+        initialRouteName="my-spot"
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          sceneStyle: { backgroundColor: 'transparent' },
+          tabBarStyle: {
+            paddingTop: 5,
+            backgroundColor: 'transparent',
+          },
+        }}>
+        <Tabs.Screen
+          name="my-spot"
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                name="house"
+                component={FontAwesome6}
+                size={22}
+                focused={focused}
+                info={t('tabs.mySpot')}
+              />
+            ),
+            tabBarButton: NoRipple,
+          }}
+        />
+        <Tabs.Screen
+          name="search-spot"
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                name="magnifying-glass"
+                component={FontAwesome6}
+                size={24}
+                focused={focused}
+                info={t('tabs.search')}
+              />
+            ),
+            tabBarButton: NoRipple,
+          }}
+        />
+        <Tabs.Screen
+          name="plans"
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabIcon
+                name="crown"
+                component={FontAwesome6}
+                size={24}
+                focused={focused}
+                info={t('tabs.upgrade')}
+              />
+            ),
+            tabBarButton: NoRipple,
+          }}
+        />
+        <Tabs.Screen
+          name="user-profile"
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Tab info={t('tabs.profile')}>
+                <MeAvatar
+                  className={cn('aspect-square h-7', focused && 'border-2 border-primary')}
                 />
-              ),
-              tabBarButton: NoRipple,
-            }}
-          />
-          <Tabs.Screen
-            name="search-spot"
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <TabIcon
-                  name="magnifying-glass"
-                  component={FontAwesome6}
-                  size={24}
-                  focused={focused}
-                  info={t('tabs.search')}
-                />
-              ),
-              tabBarButton: NoRipple,
-            }}
-          />
-          <Tabs.Screen
-            name="plans"
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <TabIcon
-                  name="crown"
-                  component={FontAwesome6}
-                  size={24}
-                  focused={focused}
-                  info={t('tabs.upgrade')}
-                />
-              ),
-              tabBarButton: NoRipple,
-            }}
-          />
-          <Tabs.Screen
-            name="user-profile"
-            options={{
-              tabBarIcon: ({ focused }) => (
-                <Tab info={t('tabs.profile')}>
-                  <MeAvatar
-                    className={cn('aspect-square h-7', focused && 'border-2 border-primary')}
-                  />
-                </Tab>
-              ),
-              tabBarButton: NoRipple,
-            }}
-          />
-        </Tabs>
-      </BottomSheetModalProvider>
+              </Tab>
+            ),
+            tabBarButton: NoRipple,
+          }}
+        />
+      </Tabs>
     </>
   );
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -13,6 +13,7 @@ import { NotificationProvider } from '~/notification/NotificationContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NAV_THEME } from '~/theme';
 import { DeviceInfo, useDeviceInfo } from '~/lib/use-device-info';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -36,13 +37,16 @@ export default function RootLayout() {
         <NotificationProvider>
           <GestureHandlerRootView style={{ flex: 1 }}>
             <NavThemeProvider value={NAV_THEME[colorScheme]}>
-              <Stack
-                initialRouteName="welcome"
-                screenOptions={{
-                  headerShown: false,
-                  animation: 'ios_from_right',
-                }}
-              />
+              <BottomSheetModalProvider>
+                {/*BottomSheetModalProvider children need to have access to the currentUser*/}
+                <Stack
+                  initialRouteName="welcome"
+                  screenOptions={{
+                    headerShown: false,
+                    animation: 'ios_from_right',
+                  }}
+                />
+              </BottomSheetModalProvider>
             </NavThemeProvider>
           </GestureHandlerRootView>
         </NotificationProvider>
