809d40155a9278adcc3e69c0366566f9a3921b63
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2025-08-28 15:20:00 +0200

    style: Update style logo

--- landing-page/src/components/Logo.tsx
+++ landing-page/src/components/Logo.tsx
@@ -6,9 +6,11 @@ export type LogoProps = Omit<ImgHTMLAttributes<HTMLImageElement>, "src">;
 export function Logo({ alt = "Main logo", ...imgProps }: LogoProps) {
   const { t } = useTranslation();
   return (
-    <a href={"/"} className="flex items-center gap-2">
-      <img src="/logo.svg" width={40} alt={alt} {...imgProps} />
-      <span className="text-xl font-bold">{t("header.logo")}</span>
-    </a>
+    <div>
+      <a href={"/"} className="inline-flex items-center gap-2">
+        <img src="/logo.svg" width={40} alt={alt} {...imgProps} />
+        <span className="text-xl font-bold">{t("header.logo")}</span>
+      </a>
+    </div>
   );
 }
